eed3ed2e416c54c33003ce6ae120517cb8b4e9f3
Harpo <harpo@harpo.me>
2022-03-22 11:55:23 -0700

    remove TwoDigit transform from bars, beats

--- src/components/clock.ts
+++ src/components/clock.ts
@@ -1,13 +1,12 @@
 import m from 'mithril'
-import { Stream } from '../types'
 import { Observable } from './components'
 import { state } from '../state'
 import { OneIndex, TwoDigit, TwoDecimal } from '../utils'
 
+export const Bars = m(Observable(OneIndex(state.bars)))
+export const Beats = m(Observable(OneIndex(state.beats)))
+export const Sixteenths = m(Observable(TwoDecimal(state.sixteenths)))
 
-export const Bars = m(Observable(TwoDigit(OneIndex(state.bars))))
-export const Beats = m(Observable(TwoDigit(OneIndex(state.beats))))
-export const Sixteenths = m(Observable(state.sixteenths))
 export const Seperator = m('', ':')
 
 export const TransportClock = m('.clock', {}, [
