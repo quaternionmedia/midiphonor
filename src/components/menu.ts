00539517fbc1820a21bffbcad5a7f488546a5561
Harpo <harpo@harpo.me>
2022-03-24 17:44:10 -0700

    selectable list!

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -1,9 +1,33 @@
 import m from 'mithril'
 import { Button, ButtonGroup, Icons, ListItem, SelectList } from 'construct-ui'
 
+let selected: String[] = []
+
 export const Menu = {
   view: vnode =>
     m(ButtonGroup, {}, [
+      m(SelectList, {
+        items: ['1', '2', '3'],
+        label: 'select',
+        trigger: m(Button, {
+          iconLeft: Icons.CHEVRONS_DOWN,
+          label: 'select MIDI device',
+        }),
+        onSelect: item => {
+          let index = selected.indexOf(item)
+          if (index > -1) {
+            selected.splice(index, 1)
+          } else {
+            selected.push(item)
+          }
+          console.log('selected', item, index)
+        },
+        itemRender: item =>
+          m(ListItem, {
+            label: item,
+            selected: selected.indexOf(item) > -1,
+          }),
+      }),
       m(Button, {
         iconLeft: Icons.APERTURE,
         label: 'button',
