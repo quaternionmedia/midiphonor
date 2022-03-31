fe138f6744c878805670bbfdb071150f819d548e
Harpo <harpo@harpo.me>
2022-03-30 22:31:21 -0700

    migrate to initial values

--- src/state.ts
+++ src/state.ts
@@ -1,7 +1,20 @@
 import { Transport, TransportTime } from 'tone'
 import { TransportState } from './types'
+import merge from 'mergerino'
+import { stream, scan } from 'flyd'
 
-import { stream } from 'flyd'
+const initial = {
+  bpm: stream(120),
+  time: stream(0),
+  bars: stream(0),
+  beats: stream(0),
+  sixteenths: stream(0),
+  transportTime: TransportTime(),
+  state: stream(Transport.state),
+}
+
+export const update = stream()
+export const states = scan(merge, initial, update)
 
 export const state: TransportState = {
   bpm: stream(120),
@@ -12,3 +25,5 @@ export const state: TransportState = {
   transportTime: TransportTime(),
   state: stream(Transport.state),
 }
+
+window.state = states
