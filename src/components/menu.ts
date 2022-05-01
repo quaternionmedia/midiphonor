12948a9a3801c7efb0b834ea217406ecc8f02bd7
Harpo <harpo@harpo.me>
2022-05-01 12:19:50 -0700

    split MidiPortSelect into own component

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -4,28 +4,7 @@ import { Button, ButtonGroup, Icons, ListItem, SelectList } from 'construct-ui'
 export const Menu = {
   view: ({ attrs: { state } }) =>
     m(ButtonGroup, {}, [
-      m(SelectList, {
-        items: ['1', '2', '3'],
-        label: 'select',
-        trigger: m(Button, {
-          iconLeft: Icons.CHEVRONS_DOWN,
-          label: 'select MIDI device',
-        }),
-        onSelect: item => {
-          let index = state().connected.indexOf(item)
-          if (index > -1) {
-            state().connected.splice(index, 1)
-          } else {
-            state().connected.push(item)
-          }
-          console.log('state.connected', item, index)
-        },
-        itemRender: item =>
-          m(ListItem, {
-            label: item,
-            selected: state().connected.indexOf(item) > -1,
-          }),
-      }),
+      m(MidiPortSelect, { state }),
       m(Button, {
         iconLeft: Icons.APERTURE,
         label: 'button',
@@ -35,4 +14,32 @@ export const Menu = {
         label: 'settings',
       }),
     ]),
+}
+
+export const MidiPortSelect = {
+  view: ({ attrs: { state } }) =>
+    m(SelectList, {
+      items: ['1', '2', '3'],
+      label: 'select',
+      trigger: m(Button, {
+        iconLeft: Icons.CHEVRONS_DOWN,
+        label: 'select MIDI device',
+      }),
+      onSelect: item => {
+        let index = state().connected.indexOf(item)
+        if (index > -1) {
+          // Found in connected list. Disconnect
+          state().connected.splice(index, 1)
+        } else {
+          // not found. Connect
+          state().connected.push(item)
+        }
+        // console.log('state.connected', item, index)
+      },
+      itemRender: item =>
+        m(ListItem, {
+          label: item,
+          selected: state().connected.indexOf(item) > -1,
+        }),
+    }),
 }
