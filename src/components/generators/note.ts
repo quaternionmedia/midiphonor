c7b55d096a78ea398127bf22c69c2569baa7147b
Harpo <harpo@harpo.me>
2022-07-18 17:21:23 -0700

    Add random note functionality to Note

--- src/components/generators/note.ts
+++ src/components/generators/note.ts
@@ -1,10 +1,27 @@
 import m from 'mithril'
-import { Synth } from 'tone'
+import { PolySynth, now } from 'tone'
+import { Button as ButtonUi } from 'construct-ui'
+// import { Button } from 'nexusui'
+import {
+  chooseDurationRandomly,
+  chooseNoteAndOctaveRandomly,
+} from '../../utils/music'
 
-const synth = new Synth().toDestination()
-export const Note = m('input[type=button]', {
-  value: 'start',
-  onclick: e => {
-    synth.triggerAttackRelease('c4', '8n')
+//define polysynth
+let polySynth = new PolySynth().toDestination()
+
+export function playNote(
+  dest = polySynth,
+  note = chooseNoteAndOctaveRandomly(),
+  duration = chooseDurationRandomly(),
+  time = now()
+) {
+  dest.triggerAttackRelease(note, duration, time)
+}
+
+export const Note = m(ButtonUi, {
+  label: 'play random note',
+  onclick: () => {
+    playNote()
   },
 })
