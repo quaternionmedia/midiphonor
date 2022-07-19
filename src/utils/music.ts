1b89a84fec8f2af7f80de2b9aa2441eacd57856d
Harpo <harpo@harpo.me>
2022-07-18 17:20:35 -0700

    Add utils/music for random note functions

---
+++ src/utils/music.ts
@@ -0,0 +1,37 @@
+/**
+Music related functions
+*/
+
+export function chooseDurationRandomly() {
+  let allDurations = ['1n', '2n', '4n', '8n', '16n', '32n', '64n']
+  return allDurations[Math.floor(Math.random() * allDurations.length)]
+}
+
+export function chooseNoteAndOctaveRandomly() {
+  //array with all the notes
+  let allNotes = [
+    'C',
+    'C#',
+    'D',
+    'D#',
+    'E',
+    'F',
+    'F#',
+    'G',
+    'G#',
+    'A',
+    'A#',
+    'B',
+  ]
+  //array with all octaves
+  let allOctaves = [2, 3, 4, 5, 6, 7]
+  //randomly choose a note and concat to octave
+  let randomNote = allNotes[Math.floor(Math.random() * allNotes.length)]
+  let randomOctave = allOctaves[Math.floor(Math.random() * allOctaves.length)]
+  //return the note
+  return randomNote + randomOctave
+}
+
+export function mapMidiKeyToFrequency(midiKey) {
+  return 440 * Math.pow(2, (midiKey - 69) / 12)
+}
