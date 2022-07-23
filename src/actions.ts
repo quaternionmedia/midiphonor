afdf2775c48ed587d8db8be83325cc20fbddbc5f
Harpo <harpo@harpo.me>
2022-07-23 16:37:39 -0700

    Migrate clockTick action to update streams directly

--- src/actions.ts
+++ src/actions.ts
@@ -1,17 +1,19 @@
 import { Transport } from 'tone'
-import { Cell } from './types'
+import { Cell, TransportState } from './types'
 
 export const Actions = {
   clockTick: (cell: Cell, t: String) => {
     console.log('clockTick', cell, t)
     let bbs = t.split(':')
+    let states: TransportState = cell.getStream()
+    states.bars(Number(bbs[0]))
+    states.beats(Number(bbs[1]))
+    states.sixteenths(Number(bbs[2]))
+    states.bpm(Transport.bpm.value)
+    states.state(Transport.state)
+
     cell.update({
       time: t,
-      bars: Number(bbs[0]),
-      beats: Number(bbs[1]),
-      sixteenths: Number(bbs[2]),
-      bpm: Transport.bpm.value,
-      state: Transport.state,
     })
   },
 }
