41ef6cb7a1548a7093ed3b2a2f5e627bc773b897
Harpo <harpo@harpo.me>
2022-07-23 16:38:21 -0700

    Update State viewer to use getStream

--- src/components/state.ts
+++ src/components/state.ts
@@ -5,6 +5,11 @@ import './syntax.css'
 State viewer
 */
 export const State = {
+  oncreate: ({ dom, attrs: { cell } }) => {
+    cell.getStream.map(s => {
+      m.render(dom, m('pre', {}, JSON.stringify(s, null, 2)))
+    })
+  },
   view: ({ attrs: { cell } }) =>
     m('pre', {}, JSON.stringify(cell.state, null, 2)),
 }
