6331a20d59a88b1b2f652d3c5ea97f9d836236c8
Harpo <harpo@harpo.me>
2022-07-23 17:10:42 -0700

    Create basic Digit component for clock digits, OneIndex digits

--- src/components/transports/clock.ts
+++ src/components/transports/clock.ts
@@ -1,14 +1,20 @@
 import m from 'mithril'
 import { o } from '../components'
-import './clock.css'
+import { OneIndex, TwoDecimal } from '../../utils/streams'
+
+export const Digit = {
+  view: ({ attrs, children }) => m('.digit', attrs, children),
+}
+
+export const Seperator = m('.seperator', {}, ':')
 
 export const TransportClock = {
   view: ({ attrs: { cell } }) =>
     m('.clock', {}, [
-      o(cell.state.bars),
-      ':',
-      o(cell.state.beats),
-      ':',
-      o(cell.state.sixteenths),
+      m(Digit, o(OneIndex(cell.state.bars))),
+      Seperator,
+      m(Digit, o(OneIndex(cell.state.beats))),
+      Seperator,
+      m(Digit, o(TwoDecimal(OneIndex(cell.state.sixteenths)))),
     ]),
 }
